9b698ea61e1ba19a1629c73fbcafececf813742e
coderwildflower <pratiemaa@gmail.com>
2021-11-27 21:31:40 +0545

    Added player rotation. Added enemy rotation

--- Assets/scripts/Guard.cs
+++ Assets/scripts/Guard.cs
@@ -12,6 +12,9 @@ public class Guard : MonoBehaviour
 
     [SerializeField]
     float holdDuration;
+
+    [SerializeField]
+    float turnSpeed;
     public float MoveSpeed
     {
         get
@@ -34,7 +37,7 @@ public class Guard : MonoBehaviour
             holdDuration = value;
         }
     }
-  
+
     public int counter = 0;
 
     // Start is called before the first frame update
@@ -46,7 +49,7 @@ public class Guard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnDrawGizmos()
@@ -56,11 +59,11 @@ public class Guard : MonoBehaviour
 
         foreach (var item in pointsToMove)
         {
-            
+
             Gizmos.DrawWireSphere(item.position, 0.3f);
             Gizmos.color = Color.green;
 
-            Gizmos.DrawLine(secondPos,item.position);
+            Gizmos.DrawLine(secondPos, item.position);
             secondPos = item.position;
         }
         Gizmos.DrawLine(secondPos, startPos);
@@ -72,7 +75,7 @@ public class Guard : MonoBehaviour
         transform.position = moveTargetPos[0].transform.position;
         while (true)
         {
-            transform.position = Vector2.MoveTowards(transform.position, moveTargetPos[counter].transform.position,moveSpeed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, moveTargetPos[counter].transform.position, moveSpeed * Time.deltaTime);
             if (Vector2.Distance(transform.position, moveTargetPos[counter].transform.position) < 0.1f)
             {
                 counter++;
@@ -81,31 +84,26 @@ public class Guard : MonoBehaviour
                     counter = 0;
                 }
                 yield return new WaitForSeconds(holdDuration);
+                yield return StartCoroutine(lookAtTarget(moveTargetPos[counter]));
             }
 
             yield return null;
         }
-       
+
     }
-    //void Patrol()
-    //{
-      
-    //    for (int i = 0; i < pointsToMove.Length; i++)
-    //    {
-    //        transform.position = Vector3.MoveTowards(transform.position, pointsToMove[counter].position, moveSpeed * Time.deltaTime);
-    //        if (Vector2.Distance(transform.position,pointsToMove[counter].position) < 1)
-    //        {
-    //            counter++;
 
-    //            if (counter > pointsToMove.Length - 1)
-    //            {
-    //                counter = 0;
-    //                transform.position = Vector3.MoveTowards(transform.position, pointsToMove[counter].position, moveSpeed * Time.deltaTime);
-    //            }
-    //        }
+    IEnumerator lookAtTarget(Transform target)
+    {
+        Vector3 dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
 
-         
-    //    }
-    //}
+        Quaternion rot = Quaternion.AngleAxis(angle, Vector3.forward);
+        while (Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.z,angle)) > 0.1f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, turnSpeed * Time.deltaTime);
+            yield return null;
+
+        }
+    }
 
 }
